f4842ed7c8d1fa1fd0811110c6b9383402c59084
Gunter Grodotzki <gunter@grodotzki.co.za>
2018-02-18 21:09:09 +0200

    init implementation (#1)

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# ansible-role-gitlab
+[![Build Status](https://travis-ci.org/NINEJKH/ansible-role-gitlab.svg?branch=master)](https://travis-ci.org/NINEJKH/ansible-role-gitlab)
+
+# NINEJKH.gitlab
+
+## Local test
+
+```bash
+$ ./test.sh -i 192.168.1.159 -u vbox
+```
+
+## Requirements
+
+* working MySQL server
+  * `SET GLOBAL innodb_file_per_table=1, innodb_file_format=Barracuda, innodb_large_prefix=1;`
+* working Redis server
+
+## Role Variables
+
+```yaml
+
+go_version: 1.8.7
+
+gitlab_version: 9-3-stable
+
+gitlab_fqdn: gitlab
+
+gitlab_root_email: root@gitlab
+
+gitlab_root_password: Foobar_2k18
+
+gitlab_unicorn_worker_processes: 3
+
+gitlab_redis_url: redis://127.0.0.1:6379
+
+gitlab_mysql_db: gitlabhq_production
+
+gitlab_mysql_user: git
+
+gitlab_mysql_pass: ''
+
+gitlab_mysql_host: 127.0.0.1
+
+gitlab_enable_ssl: yes
+
+gitlab_db_key_base:
+
+gitlab_secret_key_base:
+
+gitlab_otp_key_base:
+
+gitlab_jws_private_key:
+
+gitlab_backup_provider:
+
+gitlab_backup_region:
+
+gitlab_backup_aws_access_key_id:
+
+gitlab_backup_aws_secret_access_key:
+
+gitlab_backup_remote_directory:
+
+```
+
+## Example Playbook
+
+```yaml
+
+- hosts: gitlabs
+  roles:
+    - { role: NINEJKH.gitlab }
+```
+
+## License
+
+Licensed under the MIT License. See the [LICENSE file](LICENSE) for details.
+
+## Author Information
+
+[9JKH (Pty) Ltd.](https://9jkh.co.za)
---
+++ test.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+readlink_bin="${READLINK_PATH:-readlink}"
+if ! "${readlink_bin}" -f test &> /dev/null; then
+  __DIR__="$(dirname "$("${readlink_bin}" "${0}")")"
+else
+  __DIR__="$(dirname "$("${readlink_bin}" -f "${0}")")"
+fi
+
+# required libs
+source "${__DIR__}/.bash/functions.shlib"
+
+set -E
+trap 'throw_exception' ERR
+
+required_roles=(
+  NINEJKH.libre2
+  NINEJKH.git
+  NINEJKH.ruby
+  lifeofguenter.nodejs
+  lifeofguenter.nginx
+)
+
+usage() {
+cat <<EOF
+Usage: ${0##*/} OPTIONS
+test role
+
+    -h          display this help and exit
+    -i STRING   target host to run against (default: 127.0.0.1)
+    -T          turbo mode
+    -u STRING   connect as this user (default: current user)
+EOF
+}
+
+OPTIND=1
+while getopts "hi:Tu:" opt; do
+  case "${opt}" in
+    h )
+      usage
+      exit 0
+      ;;
+    i )
+      TARGET_HOST="${OPTARG}"
+      ;;
+    T )
+      TURBO_MODE=1
+      ;;
+    u )
+      CONNECT_USER="${OPTARG}"
+      ;;
+    '?' )
+      usage >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$((OPTIND-1))"
+
+if [[ ! -z "${TRAVIS}" ]]; then
+  unset GEM_HOME
+  unset GEM_PATH
+  unset GOROOT
+fi
+
+if [[ -z "${TARGET_HOST}" ]]; then
+  TARGET_HOST="127.0.0.1"
+  CONNECTION="local"
+fi
+
+if [[ -z "${CONNECT_USER}" ]]; then
+  CONNECT_USER="$(whoami)"
+fi
+
+if [[ -z "${CONNECTION}" ]]; then
+  CONNECTION="smart"
+fi
+
+role_root="$(pwd)"
+
+if [[ -z "${TURBO_MODE}" ]]; then
+  consolelog "installing requirements"
+  for required_role in  "${required_roles[@]}"; do
+    ansible-galaxy install "${required_role}"
+  done
+fi
+
+consolelog "running role as playbook #1"
+ansible-playbook \
+  --inventory="${TARGET_HOST}," \
+  --user="${CONNECT_USER}" \
+  --extra-vars="role_root=${role_root}" \
+  --connection="${CONNECTION}" \
+  tests/test.yml
+
+if [[ -z "${TURBO_MODE}" ]]; then
+  consolelog "running role as playbook #2"
+  ansible-playbook \
+    --inventory="${TARGET_HOST}," \
+    --user="${CONNECT_USER}" \
+    --extra-vars="role_root=${role_root}" \
+    --connection="${CONNECTION}" \
+    tests/test.yml
+fi
+
+if [[ "${CONNECTION}" == "local" ]]; then
+  echo "127.0.0.1 gitlab" | sudo tee -a /etc/hosts
+  curl -if http://gitlab
+
+  cd /home/git/gitlab
+  sudo -u git -H bundle exec rake gitlab:env:info RAILS_ENV=production
+  sudo -u git -H bundle exec rake gitlab:check RAILS_ENV=production
+fi
